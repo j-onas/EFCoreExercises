6a44c0498088136147d00b10d3f77a0c34c83c08
j-onas <jonas@hjulle.com>
2020-11-24 15:19:12 +0100

    Task7 - Error

--- EFCoreMiniExercises/Uppgifter/Task7.cs
+++ EFCoreMiniExercises/Uppgifter/Task7.cs
@@ -12,6 +12,14 @@ namespace EFCoreMiniExercises.Uppgifter
     {
         public static List<Sale> GetCustomersWithSales(Context ctx)
         {
+            //var query = ctx.Customers.Where(x => x.Sales != null).ToList();
+            //return query;
+            // Spottar ut error
+            /*
+             *  Severity	Code	Description	Project	File	Line	Suppression State
+             *  Error	CS0029	Cannot implicitly convert type 'System.Collections.Generic.List<EFCoreMiniExercises.Customer>' to 'System.Collections.Generic.List<EFCoreMiniExercises.Sale>'	EFCoreMiniExercises	E:\SKOLA\datalagring\exercises\EFCore\EFCoreMiniExercises\Uppgifter\Task7.cs	16	Active
+             */
+
             return null;
         }
     }
